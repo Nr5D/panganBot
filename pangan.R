fd71eb3bb454730d9874d120751add26652ea7a2
Abdul Aziz Nurussadad <56505862+Nr5D@users.noreply.github.com>
2022-02-04 17:11:05 +0700

    Update pangan.R

--- pangan.R
+++ pangan.R
@@ -1,5 +1,41 @@
-library('rtweet')
-library('ggplot2')
+# Read Data from Elephant SQL
+library(RPostgreSQL)
+
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv,
+                 dbname = Sys.getenv("ELEPHANT_SQL_DBNAME"), 
+                 host = Sys.getenv("ELEPHANT_SQL_HOST"),
+                 port = 5432,
+                 user = Sys.getenv("ELEPHANT_SQL_USER"),
+                 password = Sys.getenv("ELEPHANT_SQL_PASSWORD")
+)
+
+df <- dbGetQuery(con, statement = paste("SELECT * FROM Pangan"))
+
+# Looking for the Latest Data to Make Status Message
+
+library(dplyr)
+data <- df %>% 
+  filter(as.Date(date, "%d/%m/%Y") == max(as.Date(df$date, "%d/%m/%Y")))
+
+baris <- c(1:nrow(data))
+terpilih <- sample(baris, 1)
+
+dataSiap <- data %>%
+  filter(commodity == data$commodity[terpilih])
+
+dataSiap$price <- formatC(as.numeric(dataSiap$price)*1000, format="d", big.mark=".", decimal.mark=",")
+
+# Build the status message (text and URL)
+status_details <- paste0(
+  dataSiap$date[1],": Harga ", dataSiap$commodity[1],
+  " di :", "\n","\n",
+  "⛺ ",dataSiap$type[1], " adalah Rp",dataSiap$price[1],",-", "\n",
+  "🏪 ",dataSiap$type[2], " adalah Rp",dataSiap$price[2],",-", "\n",
+  "🎪 ",dataSiap$type[3], " adalah Rp",dataSiap$price[3],",-", "\n",
+  "👨🏻‍🌾 ",dataSiap$type[4], " adalah Rp",dataSiap$price[4],",-", "\n")
+
+library(rtweet)
 
 # Create Twitter token
 pangan_token <- rtweet::create_token(
@@ -10,72 +46,8 @@ pangan_token <- rtweet::create_token(
   access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 )
 
-# Read Data from previously harvested data
-data <- read.csv('https://raw.githubusercontent.com/Nr5D/panganBot/main/data/data.csv', check.names = FALSE)
-baris <- c(1:nrow(data))
-terpilih <- sample(baris, 1)
-
-# Food
-
-# Format Pakai Angka
-price <- data[,ncol(data)][terpilih]
-price <- formatC(price*1000, format="d", big.mark=".", decimal.mark=",")
-price_before <- data[,ncol(data)-1][terpilih]
-price_before <- formatC(price_before*1000, format="d", big.mark=".", decimal.mark=",")
-
-# Format Pakai Emoji
-#price <- data[,length(data)][terpilih]*1000
-#harga <- as.numeric(strsplit(as.character(price),"")[[1]])
-#dice <- 1:length(harga)
-#tes <- for (x in dice) {paste(keycap(harga[x]))}
-
-name <- data[terpilih,2]
-
-
-posisi <- function(x){
-  if ((data[x,ncol(data)]-data[x,ncol(data)-1])  > 0) {
-    print("naik")
-  } else if ((data[x,ncol(data)]-data[x,ncol(data)-1])  < 0) {
-    print("turun")
-  } else {
-    print("tetap")
-  }
-}
-
-# Build the status message (text and URL)
-status_details <- paste0(
-  colnames(data)[ncol(data)],": Harga ", name,
-  " di Pasar Tradisional adalah Rp",price,
-  ",- ",  posisi(terpilih)," dari harga sebelumnya Rp", price_before,",-"
-)
-
-# Image sample
-value = data[terpilih,3:ncol(data)]
-
-data_plot <- data.frame(
-  day = strptime(colnames(value), "%d/%m/%Y"),
-  value = t(value)*1000
-)
-
-colnames(data_plot)[2] <- data[terpilih,2]
-
-# Most basic Line and Point Plot
-p <- ggplot(data_plot, aes(x =day,  y=data_plot[,2])) +
-  geom_line(color = "#00AFBB", size = 1)+
-  geom_point(color = "#FC4E07", size = 2)+
-  xlab(colnames(data_plot)[2])+
-  ylab("Harga")+
-  scale_y_continuous(labels = function(x) paste0("Rp", formatC(x, format="d", big.mark=".", decimal.mark=","),",-" )) +
-  theme_light()
-
-# Download the image to a temporary location
-# save to a temp file
-file <- tempfile( fileext = ".png")
-ggsave(file, plot = p, device = "png", dpi = 144, width = 8, height = 8, units = "in" )
-
 # Post the image to Twitter
 rtweet::post_tweet(
   status = status_details,
-  media = file,
   token = pangan_token
 )
